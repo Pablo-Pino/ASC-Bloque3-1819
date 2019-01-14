5390757f722d7d9ff7ae16b19a6f3b42d496028e
Pablo Pino <pabpinjim@alum.us.es>
2019-01-14 18:19:55 +0100

    Primera versión del algoritmo de inicialización
    El algoritmo genera una lista de individuos, cada uno de los cuales se
    compone de una lista de números decimales aletorios

--- main.py
+++ main.py
@@ -1,13 +1,48 @@
-def main():
-    res = None
-    inicializacion()
-    for i in range(n_evaluaciones):
-        iter()
-    return res 
+from sys import argv
+from random import random
 
-def iter():
+def main():
+	n_dimensiones = int(argv[1])
+	n_individuos = int(argv[2])
+	n_evaluaciones = 10
+	res = []
+	inicializacion(n_dimensiones, n_individuos, res)
+	for i in range(n_evaluaciones):
+		iteracion()
+	for vector in res:
+		print(vector)
+	return res 
+
+def iteracion():
 	agrupar_vecindades()
 	reproduccion()
 	evaluacion()
 	actualizacion_solucion()
 	actualizacion_vecindades()
+
+def inicializacion(n_dimensiones, n_individuos, res):
+	for i in range(n_individuos):
+		res.append([])
+		for j in range(n_dimensiones):
+			res[i].append(random())
+	for elem in res:
+		print(elem)
+	
+		
+
+def agrupar_vecindades():
+	pass
+
+def reproduccion():
+	pass
+
+def evaluacion():
+	pass
+
+def actualizacion_solucion():
+	pass
+
+def actualizacion_vecindades():
+	pass
+
+main()
